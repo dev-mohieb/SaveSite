f2dbe89d43de3df5e1f6596f21c2d768f1643c5f
Mohieb <105434369+dev-mohieb@users.noreply.github.com>
2022-09-26 14:45:12 +0300

    added delete btn

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const saveBtn = document.getElementById("save-btn")
 const deleteBtn = document.getElementById("delete-btn")
 let tabsList = document.getElementById("tabs-list")
-let urlArr = []
-let titleArr = []
-const urlsFromLocalStorage = localStorage.getItem(JSON.parse(urlList))
 
-if (urlsFromLocalStorage) {
-    urlArr = urlsFromLocalStorage
-    render(urlArr, titleArr)
+
+let urlArr = []
+let titlesArr = []
+const urlsLocalStorage = localStorage.getItem(JSON.parse(urlArr))
+
+if (urlsLocalStorage) {
+    urlArr = urlsLocalStorage
+    titlesArr = titlesLocalStorage
+    render(urlArr, titlesArr)
 }
 
 saveBtn.addEventListener("click", function() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
         urlArr.push(tabs[0].url)
         localStorage.setItem("urlArr", JSON.stringify(urlArr))
-        render(urlArr, titleArr)
+        render(urlArr, titlesArr)
     })
 })
 
-function render(sitesArr, nameArr) {
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    urlArr = []
+    titlesArr = []
+    render(urlArr, titlesArr)
+})
+
+function render(site, title) {
     let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
+
+    for (let i = 0; i < site.length; i++) {
 
         listItems +=
     `
     <li>
         <div class="saved-tab">
-            <a href="${sitesArr}" target="_blank">
-                <h2>${nameArr}</h2>
+            <a href="${site}" target="_blank">
+                <h2>${title}</h2>
             </a>
         </div>
     </li>
     `
     }
+
     tabsList.innerHTML = listItems
 }
